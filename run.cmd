fbba5ca8aef3653c4a4baba499f7d8253ccce56d
LuisArmando-TestCoder <luisarmando.murillobaltodano@gmail.com>
2021-08-06 07:59:21 -0600

    Command A message

--- run.cmd
+++ run.cmd
@@ -3,10 +3,10 @@ echo off
 echo The following command is being executed
 echo . . .
 echo . . .
-echo Command: g++ -std=c++0x -o programs/p%100/p%1%20/p%1%2%3/a programs/p%100/p%1%20/p%1%2%3/a.cpp; start programs/p%100/p%1%20/p%1%2%3/a.exe
+echo Command A: g++ -std=c++0x -o programs/p%100/p%1%20/p%1%2%3/a programs/p%100/p%1%20/p%1%2%3/a.cpp; start programs/p%100/p%1%20/p%1%2%3/a.exe
 echo . . .
 echo . . .
-echo executing command above
+echo executing Command A
 echo . . .
 echo . . .
 g++ -std=c++0x -o programs/p%100/p%1%20/p%1%2%3/a programs/p%100/p%1%20/p%1%2%3/a.cpp; start programs/p%100/p%1%20/p%1%2%3/a.exe
