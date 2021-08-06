2b405e72601685a75f8fb30e69a8073c20e95e88
LuisArmando-TestCoder <luisarmando.murillobaltodano@gmail.com>
2021-08-06 08:11:34 -0600

    Add run.cmd commands

--- run.cmd
+++ run.cmd
@@ -1,13 +1,8 @@
 echo off
-echo The following command is being executed
-echo . . .
-echo . . .
-echo Command A: g++ -std=c++0x -o programs/p%100/p%1%20/p%1%2%3/a programs/p%100/p%1%20/p%1%2%3/a.cpp; start programs/p%100/p%1%20/p%1%2%3/a.exe
-echo . . .
-echo . . .
-echo executing Command A
-echo . . .
-echo . . .
+echo The following commands are executing at %~dp0
 
+echo g++ -std=c++0x -o programs/p%100/p%1%20/p%1%2%3/a programs/p%100/p%1%20/p%1%2%3/a.cpp
 g++ -std=c++0x -o programs/p%100/p%1%20/p%1%2%3/a programs/p%100/p%1%20/p%1%2%3/a.cpp
+
+echo start programs/p%100/p%1%20/p%1%2%3/a.exe
 start programs/p%100/p%1%20/p%1%2%3/a.exe
