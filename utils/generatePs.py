54b1d6b8ca48ac6d02cc34a12ff314988294b88f
LuisArmando-TestCoder <luisarmando.murillobaltodano@gmail.com>
2021-08-05 22:43:02 -0600

    CPP Tutorial Generate Better Structures

--- utils/generatePs.py
+++ utils/generatePs.py
@@ -6,7 +6,7 @@ population = 10
 for i0 in range(0, step * population, step):
     for i1 in range(0, step, int(step / population)):
         for i2 in range(int(step / population)):
-            folderPath = f"programs/p{i0}/p{i1}/p{i2}"
+            folderPath = f"programs/p{i0}/p{i0 + i1}/p{i0 + i1 + i2}"
 
             if not os.path.exists(folderPath):
                 os.makedirs(folderPath)
