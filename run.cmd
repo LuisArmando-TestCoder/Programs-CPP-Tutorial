245fc0056810e9fcf1830b82829f4edf6e08bfc6
LuisArmando-TestCoder <luisarmando.murillobaltodano@gmail.com>
2021-08-06 21:57:04 -0600

    Enhance run.cmd

--- run.cmd
+++ run.cmd
@@ -5,11 +5,11 @@ echo Executing at %0
 set index=%1
 set baseFolders=programs/%index:~0,1%00/%index:~0,1%%index:~1,1%0/%index:~0,1%%index:~1,1%%index:~2,1%
 
-echo Creating %baseFolders% and program.cpp, if needed
+echo If non existent, creating %baseFolders%/program.cpp
 
 py utils/generateProgram.py %index:~0,1% %index:~1,1% %index:~2,1%
 
-echo Compiling %baseFolders%/program.cpp
+echo Compiling C++ code at %baseFolders%/program.cpp
 
 g++ -std=c++0x -o %baseFolders%/program %baseFolders%/program.cpp
 
