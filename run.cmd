4dd92d6f703e431a9babbb50719187bb801b86f1
LuisArmando-TestCoder <luisarmando.murillobaltodano@gmail.com>
2021-08-06 21:37:48 -0600

    add baseFolders to cmd command

--- run.cmd
+++ run.cmd
@@ -5,21 +5,22 @@ echo Executing at %0
 echo . . .
 
 set index=%1
+set baseFolders=programs/%index:~0,1%00/%index:~0,1%%index:~1,1%0/%index:~0,1%%index:~1,1%%index:~2,1%
 
-echo A: Creating "programs/%index:~0,1%00/%index:~0,1%%index:~1,1%0/%index:~0,1%%index:~1,1%%index:~2,1%/program.cpp" if needed
+echo A: Creating %baseFolders%/program.cpp if needed
 
 py utils/generateProgram.py %index:~0,1% %index:~1,1% %index:~2,1%
 
 echo . . .
 
-echo B: Compiling "programs/%index:~0,1%00/%index:~0,1%%index:~1,1%0/%index:~0,1%%index:~1,1%%index:~2,1%/program.cpp"
+echo B: Compiling %baseFolders%/program.cpp
 
-g++ -std=c++0x -o programs/%index:~0,1%00/%index:~0,1%%index:~1,1%0/%index:~0,1%%index:~1,1%%index:~2,1%/program programs/%index:~0,1%00/%index:~0,1%%index:~1,1%0/%index:~0,1%%index:~1,1%%index:~2,1%/program.cpp
-
-echo . . .
-
-echo C: Starting "programs/%index:~0,1%00/%index:~0,1%%index:~1,1%0/%index:~0,1%%index:~1,1%%index:~2,1%/program.exe"
+g++ -std=c++0x -o %baseFolders%/program %baseFolders%/program.cpp
 
 echo . . .
 
-start programs/%index:~0,1%00/%index:~0,1%%index:~1,1%0/%index:~0,1%%index:~1,1%%index:~2,1%/program.exe
+echo C: Starting %baseFolders%/program.exe
+
+echo . . .
+
+start %baseFolders%/program.exe
